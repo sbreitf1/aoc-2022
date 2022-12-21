1911a0d0804f116c94ff6952f114534f8ed0b36d
Simon Breitfelder <sbreitf1@web.de>
2022-12-21 18:44:17 +0100

    added puzzle 7 basics

---
+++ puzzle-7/main.go
@@ -0,0 +1,16 @@
+package main
+
+// https://adventofcode.com/2022/day/7
+
+import (
+	"aoc/helper"
+	"fmt"
+)
+
+func main() {
+	lines := helper.ReadLines("example.txt")
+	fmt.Println(lines)
+
+	fmt.Println("-> part 1:", nil)
+	fmt.Println("-> part 2:", nil)
+}
